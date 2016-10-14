633a8501209478cf9d9d3a835008ddd082908c78
Smirnova Aleksandra <smirnova.a.yu@gmail.com>
2016-10-15 00:18:55 +0300

    fixed json + host and port

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+import json
+import sys
 from flask import Flask, render_template
 from flask_socketio import join_room, leave_room, send, SocketIO
 
 import conf
+from utils import decode_json
 
 app = Flask(__name__)
 app.debug = conf.DEBUG
@@ -15,15 +19,17 @@ def hello():
 
 
 @socketio.on('join')
-def on_join(data):
+def on_join(request):
+    data = decode_json(json.loads(request))
     username = data['username']
     room = data['room']
     join_room(room)
-    send(username + ' has entered the room.', room=room)
+    send(json.dumps('{0} has entered the room.'.format(username)), room=room)
 
 
 @socketio.on('leave')
-def on_leave(data):
+def on_leave(request):
+    data = decode_json(json.loads(request))
     username = data['username']
     room = data['room']
     leave_room(room)
@@ -31,10 +37,18 @@ def on_leave(data):
 
 
 @socketio.on('message')
-def message_for_room(data):
+def message_for_room(request):
+    data = decode_json(json.loads(request))
     room = data['room']
-    send('received data: ' + str(data), room=room)
+    send(json.dumps(json.dumps(data)), room=room)
 
 
 if __name__ == '__main__':
-    socketio.run(app, host=conf.HOST)
+    if len(sys.argv) < 3:
+        host = conf.HOST
+        port = conf.PORT
+    else:
+        host = sys.argv[1]
+        port = sys.argv[2]
+
+    socketio.run(app, host=host, port=int(port))
